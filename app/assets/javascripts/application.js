e40c548f0dccda657e3f301e263999906b12a4a6
katjaalcock <114230664+katjaalcock@users.noreply.github.com>
2023-04-13 16:18:45 +0100

    Stay up to date

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,27 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+
+    // Select and loop the container element of the elements you want to equalise
+    $('.govuk-grid-row').each(function(){  
+      
+      // Cache the highest
+      var highestBox = 0;
+      
+      // Select and loop the elements you want to equalise
+      $('.card-container', this).each(function(){
+        
+        // If this box is higher than the cached highest then store it
+        if($(this).height() > highestBox) {
+          highestBox = $(this).height(); 
+        }
+      
+      });  
+            
+      // Set the height of all those children to whichever was highest 
+      $('.card-container',this).height(highestBox);
+                    
+    }); 
+
 })
+
